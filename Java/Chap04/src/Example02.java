626c70b822345d50c6275d98c964b07859cd645b
Marble_ly <385516@naver.com>
2023-09-25 17:07:44 +0900

    북마크 프로젝트 챕터 3 업로드

---
+++ Java/Chap04/src/Example02.java
@@ -0,0 +1,16 @@
+
+public class Example02 {
+
+	public static void main(String[] args) {
+		int num = 120;
+		
+		if (num < 50) {
+			System.out.println("숫자가 50보다 작습니다");
+		}
+		else {
+			System.out.println("숫자가 50보다 큽니다.");
+		}
+
+	}
+
+}
