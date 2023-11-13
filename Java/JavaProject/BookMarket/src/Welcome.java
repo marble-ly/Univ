7f0dca1630adb6b99645507098243f6e99e02a3f
marble-ly <385516@naver.com>
2023-11-13 16:15:48 +0900

    1113upload

--- Java/JavaProject/BookMarket/src/Welcome.java
+++ Java/JavaProject/BookMarket/src/Welcome.java
@@ -79,7 +79,9 @@ public class Welcome {
 	
 	public static void menuGuestInfo(String name, int mobile) {
 		System.out.println("현재 고객 정보 : ");
-		System.out.println("이름 " + name + " 연락처 " + mobile);
+		//System.out.println("이름 " + name + " 연락처 " + mobile);
+		Person person = new Person(name, mobile);
+		System.out.println("이름 " + person.getName() + " 연락처 " + person.getPhone());
 	}
 	
 	public static void menuCartItemList() {
